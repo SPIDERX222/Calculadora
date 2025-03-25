510de64abcafaf330aeee671b042d54b9710cbd2
AlumMati <AlumMati@PC08>
2025-03-25 08:38:39 +0100

    Implementación de los metodos restar y multiplicar

--- Calculadora/Calculadora.java
+++ Calculadora/Calculadora.java
@@ -4,6 +4,12 @@ public class Calculadora {
 	 public int sumar(int a, int b) {
 		 return a + b;
 	 }
+	 public int restar(int a, int b) {
+		 return a - b;
+		}
+		public int multiplicar(int a, int b) {
+		 return a * b;
+		}
 	 
 	 public static void main(String[] args) {
 		 Calculadora calc = new Calculadora();
