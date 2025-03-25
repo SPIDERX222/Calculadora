b11fc2cb5edf4fc380882f8b9bcbe0ea214393df
AlumMati <AlumMati@PC08>
2025-03-25 08:47:25 +0100

    Implemantacion restar y multiplicar

--- Calculadora/Calculadora.java
+++ Calculadora/Calculadora.java
@@ -13,4 +13,4 @@ public class Calculadora {
 	 
 	 public static void main(String[] args) {
 		 Calculadora calc = new Calculadora();
-		 System.out.println("Resultado de la suma: " +calc.sumar(5, 3));}}
+		 System.out.println("Resultado de la suma: " +calc.sumar(7, 3));}}
